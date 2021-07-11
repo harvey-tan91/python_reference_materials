cdc6db4605f5703a0bc3fb407951b4ffeb04682b
Harvey Tan <tanzhengming91@gmail.com>
2021-07-11 17:48:36 +0800

    new materials update

--- reference_materials/how_to_download_content_from_web.py
+++ reference_materials/how_to_download_content_from_web.py
@@ -4,10 +4,13 @@ import requests
 from pandas import json_normalize
 
 content = requests.get(r'http://pythonscraping.com/pages/page1.html') # this return a Response object
-print(content.text)
-print(content.status_code)
+print(content.text) # this return a string obj
+print(content.content) # this return the result in bytes datatype
+print()
+
+print(content.status_code) # refer to < https://en.wikipedia.org/wiki/List_of_HTTP_status_codes > 
 print(content.encoding)
-print(content.headers)
+print(content.headers) 
 
 #%%
 json_content = requests.get(r'https://www.tablebuilder.singstat.gov.sg/publicfacing/api/json/title/15694.json')
@@ -15,3 +18,4 @@ json_content = json_content.json()
 json_normalize(json_content)
 #%%
 json_normalize(json_content, 'Level1')
+
